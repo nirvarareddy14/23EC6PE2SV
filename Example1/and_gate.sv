329b0ccd5864a6d52c4dd162aa968c633f8877a1
Nirvara Reddy <nirvara.reddy@gmail.com>
2026-01-29 00:59:03 +0530

    Implement testbench with coverage for and_gate
    Added a testbench module with coverage for the and_gate.

--- Example1/and_gate.sv
+++ Example1/and_gate.sv
@@ -1,1 +1,26 @@
+module tb;
+   logic a, b, y;
+   and_gate dut(.*); // Connect by name
 
+   // Covergroup defines what we want to measure
+   covergroup cg_and;
+     cp_a: coverpoint a;
+     cp_b: coverpoint b;
+     cross_ab: cross cp_a, cp_b; // The most important line
+   endgroup
+
+   cg_and cg=new(); // Create instance
+
+   initial begin
+     $dumpfile("dump.vcd");
+     $dumpvars;
+     
+     repeat(20) begin
+     a=$urandom(); // Randomize 0 or 1
+     b=$urandom();
+     #5; // Wait for gate delay
+     cg.sample(); // Tell coverage tool to record values
+     end
+     $display ("Final Coverage = %0.2f %%", cg.get_inst_coverage());
+   end
+endmodule
