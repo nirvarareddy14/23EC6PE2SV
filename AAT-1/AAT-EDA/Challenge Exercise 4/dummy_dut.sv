e476579d52899348c148be50d356676504c29452
Nirvara Reddy <nirvara.reddy@gmail.com>
2026-02-13 19:14:34 +0530

    Update and rename _ to dummy_dut.sv

---
+++ AAT-1/AAT-EDA/Challenge Exercise 4/dummy_dut.sv
@@ -0,0 +1,12 @@
+//------------------------------------------------------------------------------
+//File       : dummy_dut.sv
+//Author     : Nirvara Reddy/1BM23EC170
+//Created    : 2026-02-10
+//Module     : dummy_dut
+//Project    : SystemVerilog and Verification (23EC6PE2SV)
+//Faculty    : Prof. Ajaykumar Devarapalli
+//Description: A placeholder dummy DUT for the class-based Packet verification lab.
+//------------------------------------------------------------------------------
+
+module dummy_dut;
+endmodule
