64aca5601358cd9909d5b05674699c9801072631
Nirvara Reddy <nirvara.reddy@gmail.com>
2026-02-28 23:27:52 +0530

    Create clock_interface.sv

---
+++ AAT-2/AAT-Cadence/Digital Clock/clock_interface.sv
@@ -0,0 +1,32 @@
+// -----------------------------------------------------------------------------
+// File        : clock_interface.sv
+// Author      : Nirvara Reddy (1BM23EC170)
+// Created     : 2026-02-28
+// Module      : clock_interface
+// Project     : SystemVerilog and Verification (23EC6PE2SV),
+//               Faculty: Prof. Ajaykumar Devarapalli
+// Description : Digital clock interface module
+// -----------------------------------------------------------------------------
+interface clock_interface (input logic clk);
+
+    logic reset;
+    logic [5:0] seconds;
+    logic [5:0] minutes;
+
+    // Modport for DUT
+    modport DUT (
+        input  clk,
+        input  reset,
+        output seconds,
+        output minutes
+    );
+
+    // Modport for Testbench
+    modport TB (
+        input  clk,
+        output reset,
+        input  seconds,
+        input  minutes
+    );
+
+endinterface
