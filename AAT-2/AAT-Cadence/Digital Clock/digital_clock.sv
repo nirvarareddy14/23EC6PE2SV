77d91637c01ec86af6c1a1a513cb6958203fdf0c
Nirvara Reddy <nirvara.reddy@gmail.com>
2026-02-28 23:22:36 +0530

    Update digital_clock.sv

--- AAT-2/AAT-Cadence/Digital Clock/digital_clock.sv
+++ AAT-2/AAT-Cadence/Digital Clock/digital_clock.sv
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // File        : digital_clock.sv
-// Author      : Nirvara Reddy
+// Author      : Nirvara Reddy (1BM23EC170)
 // Created     : 2026-02-28
 // Module      : digital_clock
 // Project     : SystemVerilog and Verification (23EC6PE2SV),
