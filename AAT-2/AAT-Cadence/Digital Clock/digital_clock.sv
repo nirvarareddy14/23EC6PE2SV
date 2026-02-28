8ab8f31ae4b92bd9fa68631e594c64ea7b6bf026
Nirvara Reddy <nirvara.reddy@gmail.com>
2026-02-28 23:12:15 +0530

    Create digital_clock.sv

---
+++ AAT-2/AAT-Cadence/Digital Clock/digital_clock.sv
@@ -0,0 +1,38 @@
+// -----------------------------------------------------------------------------
+// File        : digital_clock.sv
+// Author      : Nirvara Reddy
+// Created     : 2026-02-28
+// Module      : digital_clock
+// Project     : SystemVerilog and Verification (23EC6PE2SV),
+//               Faculty: Prof. Ajaykumar Devarapalli
+//
+// Description : A clock is implemented using two synchronized 6-bit counters — 
+//              one for seconds and one for minutes — with each counter incrementing from 0 to 59. 
+// -----------------------------------------------------------------------------
+
+module digital_clock (
+    input  logic        clk,
+    input  logic        reset,        // Active-high synchronous reset
+    output logic [5:0]  seconds,
+    output logic [5:0]  minutes
+);
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            seconds <= 6'd0;
+            minutes <= 6'd0;
+        end
+        else begin
+            if (seconds == 6'd59) begin
+                seconds <= 6'd0;
+
+                if (minutes == 6'd59)
+                    minutes <= 6'd0;
+                else
+                    minutes <= minutes + 6'd1;
+            end
+            else begin
+                seconds <= seconds + 6'd1;
+            end
+        end
+    end
+endmodule
